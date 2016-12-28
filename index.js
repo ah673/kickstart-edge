2caea23d63054d9965eead80a7ccd4f5e93d01f4
Amy Ho <ah673@cornell.edu>
2016-12-27 21:35:30 -0500

    scraping of pledge titles

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
+// make environment variable or user input
+const url = 'https://www.kickstarter.com/projects/poots/kingdom-death-monster-15';
+const pledgeTiers = ['Frogdog'];
+
+
+
 const express = require('express');
+const request = require('request');
+const artoo = require('artoo-js');
+const cheerio = require('cheerio');
+
 const app = express();
 
 app.get('/', function (req, res) {
-  res.writeHead(200);
-  res.end('Hello world');
+
+  request(url, function(error, response, html) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    parseForTiers(html, pledgeTiers);
+    res.writeHead(200);
+    res.end(html);
+
+  });
+
 });
+
+function parseForTiers (html, pledgeTiers) {
+  var $ = cheerio.load(html);
+  artoo.setContext($);
+
+  var list = artoo.scrape('.NS_projects__rewards_list li.hover-group', {
+    pledgeLevel: {
+      sel: '.pledge__title',
+      method: 'text'
+    }
+
+  });
+  console.log(list);
+
+}
 
 app.listen('8000');
 
